d41363dded909d1eb0ab3574f4398132f982fb52
rhymus314 <yuys03145@gmail.com>
2022-09-15 11:21:08 +0900

    kakaoStrategy

--- backend/passport/kakaoStrategy.js
+++ backend/passport/kakaoStrategy.js
@@ -13,9 +13,10 @@ module.exports = () => {
             },
             async (accessToken, refreshToken, profile, done) => {
                 // console.log('kakao profile', profile);
-                console.log(profile._json.kakao_account.email)
-                console.log(accessToken)
-                console.log(refreshToken)
+                console.log(profile)
+                // console.log(profile._json.kakao_account.email)
+                // console.log(accessToken)
+                // console.log(refreshToken)
                 try{
                     const exUser = await User.findOne({
                         where: { id : profile.id},
@@ -28,10 +29,9 @@ module.exports = () => {
                             id: profile.id,
                             email: profile._json.kakao_account.email,
                             name: profile.displayName,
-                            password : accessToken,//'7XXSpVbjANfOtG9cFxQXVFNcCIRQ0Zmf0lECeit-JwgudvGTg8h5ApiiSMcigRrI52HMAAorDSAAAAGDPF-8ow',
+                            password : accessToken,
                             createdAt : profile.connected_at,
                             updatedAt : profile.connected_at,
-                            //provider: 'kakao',
                         });
                         done(null, newUser);
                     }
@@ -43,46 +43,3 @@ module.exports = () => {
         ),
     );
 };
-
-// import { Strategy as KakaoStrategy } from "passport-kakao";
-// import { User } from "../models";
-
-// // 2. 카카오 로그인 (콜백 함수 실행 안 됨)
-// // 4. 콜백 함수 실행 됨
-// passport.use(
-//   new KakaoStrategy(
-//     {
-//       clientID: '69573529168e45d59663e2888a63c906',
-//       clientSecret: "", // clientSecret을 사용하지 않는다면 넘기지 말거나 빈 스트링을 넘길 것
-//       callbackURL: '/auth/kakao/callback',
-//     },
-//     async (_, __, profile, cb) => {
-//       const {
-//         id,
-//         username: name,
-//         _json: {
-//           properties: { profile_image },
-//           kakao_account: { email },
-//         },
-//       } = profile;
-//       try {
-//         const user = await User.findOne({ email });
-//         if (user) {
-//           user.kakaoId = id;
-//           user.save();
-//           return cb(null, user);
-//         } else {
-//           const newUser = await User.create({
-//             email,
-//             name,
-//             kakaoId: id,
-//             avartarUrl: profile_image,
-//           });
-//           return cb(null, newUser);
-//         }
-//       } catch (error) {
-//         return cb(error);
-//       }
-//     }
-//   )
-// );
